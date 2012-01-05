46242cf5479f4e0a3578d894667164f0569afaa8
Daniel Gollub <gollub@b1-systems.de>
2012-01-05 10:29:22 +0100

    Fix pacemaker ticket handling
    Use crm_ticket with -v not -n as suggested by Gaoyan.
    Reported by  Joerg Frede.
    
    Signed-off-by: Daniel Gollub <gollub@b1-systems.de>

--- src/pacemaker.c
+++ src/pacemaker.c
@@ -27,7 +27,7 @@ static void pcmk_grant_ticket(const void *ticket)
 	FILE *p;
 	char cmd[COMMAND_MAX];
 
-	snprintf(cmd, COMMAND_MAX, "crm_ticket -t %s -n true", (char *)ticket);
+	snprintf(cmd, COMMAND_MAX, "crm_ticket -t %s -v true", (char *)ticket);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
 	if (p == NULL) {
@@ -44,7 +44,7 @@ static void pcmk_revoke_ticket(const void *ticket)
 	FILE *p;
 	char cmd[COMMAND_MAX];
 
-	snprintf(cmd, COMMAND_MAX, "crm_ticket -t %s -n false", (char *)ticket);
+	snprintf(cmd, COMMAND_MAX, "crm_ticket -t %s -v false", (char *)ticket);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
 	if (p == NULL) {
