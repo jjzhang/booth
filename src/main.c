47c0ef68d24d57a1adcc81b2a0ef9cf9013450db
Yuich SEINO <seino.cluster2@gmail.com>
2012-05-28 17:53:23 +0900

    Fixed:tcp listener error and trivial fix

--- src/main.c
+++ src/main.c
@@ -611,7 +611,7 @@ static int do_command(cmd_request_t cmd)
 	}
 	
 	if (reply.result == BOOTHC_RLT_OVERGRANT) {
-		log_info("You're granting a granted ticket"
+		log_info("You're granting a granted ticket "
 			 "If you wanted to migrate a ticket,"
 			 "use revoke first, then use grant");
 		rv = -1;
