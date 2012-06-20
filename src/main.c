jjzhang/booth

commit c93995ed3e6d8ddf1d11ac7d9f8ed4c1a3f3ad73

Make daemonization happen later, after setup (bnc#739015)

src/main.c
@@ -977,6 +977,10 @@ static int do_server(int type)
 	int fd;
 	int rv = -1;
 
+	rv = setup(SITE);
+	if (rv < 0)
+		goto out;
+
 	if (!debug_level) {
 		if (daemon(0, 0) < 0) {
 			perror("daemon error");
@@ -984,6 +988,10 @@ static int do_server(int type)
 		}
 	}
 
+	/*
+	  The lock cannot be obtained before the call to daemon(), otherwise
+	  the lockfile would contain the pid of the parent, not the daemon.
+	*/
 	fd = lockfile();
 	if (fd < 0)
 		return fd;
@@ -996,10 +1004,9 @@ static int do_server(int type)
 	set_scheduler();
 	set_oom_adj(-16);
 
-	rv = setup(SITE);
-	if (rv == 0)
-		rv = loop();
+	rv = loop();
 
+out:
 	unlink_lockfile(fd);
 
 	return rv;

commit 4b00d46480f45a205f2550ff0760c8b372009f7f

Fix the wrong error message when multiple booth daemons being started.
This patch fixes the regression which was introduced by commit c93995ed,
we move all of the initialization before daemonization happens, thus lost
the protection which was provided by lockfile(). So fix it now.

Reported-and-Tested-by: Yuich SEINO <seino.cluster2@gmail.com>
Signed-off-by: Jiaju Zhang <jjzhang@suse.de>

src/main.c
@@ -447,14 +447,12 @@ static int setup_timer(void)
 	return timerlist_init();
 }
 
-static int setup(int type)
+static int loop(void)
 {
-	int rv;
-
-	rv = setup_config(type);
-	if (rv < 0)
-		goto fail;
-
+	void (*workfn) (int ci);
+	void (*deadfn) (int ci);
+	int rv, i;
+	
 	rv = setup_timer();
 	if (rv < 0)
 		goto fail;
@@ -471,18 +469,6 @@ static int setup(int type)
 	if (rv < 0)
 		goto fail;
 	client_add(rv, process_listener, NULL);
-
-	return 0;
-
-fail:
-	return -1;
-}
-
-static int loop(void)
-{
-	void (*workfn) (int ci);
-	void (*deadfn) (int ci);
-	int rv, i;
 
         while (1) {
                 rv = poll(pollfd, client_maxi + 1, poll_timeout);
@@ -947,7 +933,7 @@ static int do_server(int type)
 	int fd = -1;
 	int rv = -1;
 
-	rv = setup(type);
+	rv = setup_config(type);
 	if (rv < 0)
 		goto out;
 