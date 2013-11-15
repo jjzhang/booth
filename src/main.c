6c53e43552bb85acce8939c52f9585fb6ac74fe8
Philipp Marek <philipp.marek@linbit.com>
2013-11-15 13:19:04 +0100

    Whitespace fixes.

--- src/main.c
+++ src/main.c
@@ -702,42 +702,42 @@ static int do_revoke(void)
 
 static int _lockfile(int mode, int *fdp, pid_t *locked_by)
 {
-    struct flock lock;
-    int fd;
+	struct flock lock;
+	int fd;
 
 
-    /* After reboot the directory may not yet exist.
-     * Try to create it, but ignore errors. */
-    if (strncmp(cl.lockfile, BOOTH_RUN_DIR,
-		strlen(BOOTH_RUN_DIR)) == 0)
-	mkdir(BOOTH_RUN_DIR, 0775);
+	/* After reboot the directory may not yet exist.
+	 * Try to create it, but ignore errors. */
+	if (strncmp(cl.lockfile, BOOTH_RUN_DIR,
+				strlen(BOOTH_RUN_DIR)) == 0)
+		mkdir(BOOTH_RUN_DIR, 0775);
 
 
-    if (locked_by)
+	if (locked_by)
 		*locked_by = 0;
 
-    *fdp = -1;
-    fd = open(cl.lockfile, mode, 0664);
-    if (fd < 0)
+	*fdp = -1;
+	fd = open(cl.lockfile, mode, 0664);
+	if (fd < 0)
 		return errno;
 
-    *fdp = fd;
+	*fdp = fd;
 
-    lock.l_type = F_WRLCK;
-    lock.l_start = 0;
-    lock.l_whence = SEEK_SET;
-    lock.l_len = 0;
-    lock.l_pid = 0;
+	lock.l_type = F_WRLCK;
+	lock.l_start = 0;
+	lock.l_whence = SEEK_SET;
+	lock.l_len = 0;
+	lock.l_pid = 0;
 
 
-    if (fcntl(fd, F_SETLK, &lock) == 0)
-	return 0;
+	if (fcntl(fd, F_SETLK, &lock) == 0)
+		return 0;
 
-    if (locked_by)
-	if (fcntl(fd, F_GETLK, &lock) == 0)
-	    *locked_by = lock.l_pid;
+	if (locked_by)
+		if (fcntl(fd, F_GETLK, &lock) == 0)
+			*locked_by = lock.l_pid;
 
-    return errno;
+	return errno;
 }
 
 
