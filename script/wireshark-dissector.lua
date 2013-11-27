986f6d5fdedecba8cd496c20761d6df5851b0b9c
Philipp Marek <philipp.marek@linbit.com>
2013-11-27 15:21:40 +0100

    Dissector updates.

--- script/wireshark-dissector.lua
+++ script/wireshark-dissector.lua
@@ -10,30 +10,35 @@ do
 
 	function booth_proto.dissector(buffer, pinfo, tree)
 		local endbuf = buffer:len()
-
-		local cmd = buffer(16,4)
-
-		local hdr = tree:add(booth_proto, buffer(0, 24), "Booth header")
-		T32(hdr, buffer,  0, "Magic   %08x");
-		T32(hdr, buffer,  4, "Version %08x");
-		T32(hdr, buffer,  8, "From    %08x");
-		T32(hdr, buffer, 12, "Length  %8d");
-		T32(hdr,    cmd,  0, "Cmd     %08x, \"" .. cmd:string() .. "\"");
-		T32(hdr, buffer, 20, "Result  %08x");
-
-		if (endbuf > 24) then
-			local tick = tree:add(booth_proto, buffer(24, endbuf-24), "Booth data")
-			local name = buffer(24, 64)
-			tick:add(name,                "Ticket name: ", name:string())
-
-			T32(tick, buffer, 24+64 +  0, "Owner:        %08x")
-			T32(tick, buffer, 24+64 +  4, "Ballot:       %08x")
-			T32(tick, buffer, 24+64 +  8, "Prev. Ballot: %08x")
-			T32(tick, buffer, 24+64 + 12, "Expiry:       %8d")
-		end
-
-		pinfo.cols.info = "Booth, cmd " .. cmd:string()
 		pinfo.cols.protocol = "Booth"
+
+		if (endbuf < 24) then
+			pinfo.cols.info = "Booth - too small"
+		else
+			local cmd = buffer(16,4)
+
+			local hdr = tree:add(booth_proto, buffer(0, 24), "Booth header")
+			T32(hdr, buffer,  0, "Magic   %08x");
+			T32(hdr, buffer,  4, "Version %08x");
+			T32(hdr, buffer,  8, "From    %08x");
+			T32(hdr, buffer, 12, "Length  %8d");
+			T32(hdr,    cmd,  0, "Cmd     %08x, \"" .. cmd:string() .. "\"");
+			T32(hdr, buffer, 20, "Result  %08x");
+
+			if (endbuf > 24) then
+				local tick = tree:add(booth_proto, buffer(24, endbuf-24), "Booth data")
+				local name = buffer(24, 64)
+				tick:add(name,                "Ticket name: ", name:string())
+
+				T32(tick, buffer, 24+64 +  0, "Owner:        %08x")
+				T32(tick, buffer, 24+64 +  4, "Ballot:       %08x")
+				T32(tick, buffer, 24+64 +  8, "Prev. Ballot: %08x")
+				T32(tick, buffer, 24+64 + 12, "Expiry:       %8d")
+			end
+
+			pinfo.cols.info = "Booth, cmd " .. cmd:string()
+		end
+		tree:add(booth_proto, buffer(0, endbuf), "data")
 	end
 
 	local tbl = DissectorTable.get("udp.port")
