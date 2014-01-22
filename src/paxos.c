jjzhang/booth

commit 515333f1cf5be0c36d905523cb1646a07ca4542b

Silently ignore a delayed message.
Happens if we get the same proposal again, but already have the state.

src/paxos.c
@@ -283,6 +283,10 @@ accepting:
 				tk->name, from->addr_string, ballot,
 				ticket_owner_string(new_owner));
 		change_ticket_owner(tk, ballot, new_owner);
+	} if (ballot == tk->last_ack_ballot &&
+			ballot == tk->new_ballot &&
+			ntohl(msg->ticket.prev_ballot) == tk->last_ack_ballot) {
+		/* Silently ignore delayed messages. */
 	} else {
 		msg->header.cmd         = htonl(OP_REJECTED);
 		msg->ticket.ballot      = htonl(tk->new_ballot);

commit 89f9ba6e0b15aca738a2855535f0ba088c4f1c7f

Bugfix: Don't send OP_REJECTING for a valid OP_PROPOSING.
Bug introduced in 515333f.

src/paxos.c
@@ -378,7 +378,7 @@ accepting:
 				tk->name, from->addr_string, ballot,
 				ticket_owner_string(new_owner));
 		change_ticket_owner(tk, ballot, new_owner);
-	} if (ballot == tk->last_ack_ballot &&
+	} else if (ballot == tk->last_ack_ballot &&
 			ballot == tk->new_ballot &&
 			ntohl(msg->ticket.prev_ballot) == tk->last_ack_ballot) {
 		/* Silently ignore delayed messages. */