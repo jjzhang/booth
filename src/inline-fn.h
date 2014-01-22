3dfc2ab4114892b0efa48401141c022860e6fdb5
Philipp Marek <philipp.marek@linbit.com>
2014-01-22 10:24:28 +0100

    Fixed (C)opyright year in files touched that year.

--- src/inline-fn.h
+++ src/inline-fn.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2013 Philipp Marek <philipp.marek@linbit.com>
+ * Copyright (C) 2013-2014 Philipp Marek <philipp.marek@linbit.com>
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
