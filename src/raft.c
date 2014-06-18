a21dbda7bcd720d325905c01cbbd6715283e19fc
Dejan Muhamedagic <dejan@suse.de>
2014-06-18 10:47:24 +0200

    Medium: raft: reset ticket leader before elections (bnc#883150)
    The server will refuse heartbeat from the newly elected leader in
    case the ticket leader is set to local. This may happen if the
    server finds itself in the CIB as the ticket leader, but then
    still loses elections, because somebody else already started
    them.

--- src/raft.c
+++ src/raft.c
@@ -729,6 +729,7 @@ int new_election(struct ticket_config *tk,
 	record_vote(tk, local, new_leader);
 	tk->voted_for = new_leader;
 
+	tk->leader = no_leader;
 	tk->state = ST_CANDIDATE;
 
 	/* some callers may want just to repeat on timeout */
@@ -898,7 +899,7 @@ int raft_answer(
 		break;
 	case OP_HEARTBEAT:
 		if (tk->leader != local &&
-				(tk->state == ST_INIT ||tk->state == ST_FOLLOWER ||
+				(tk->state == ST_INIT || tk->state == ST_FOLLOWER ||
 				tk->state == ST_CANDIDATE))
 			rv = answer_HEARTBEAT(tk, sender, leader, msg);
 		else {
